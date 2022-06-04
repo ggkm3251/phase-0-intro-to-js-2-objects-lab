fd5f0c1b8ec0f88d5f015d1dc248996cd521d523
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-04 18:26:17 +0300

    commit tree 07d503cc4f3ddb07f2f73ac16cfa7545ab317bc9

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    "name": 'Glenn',
+    "streetAddress": "Thika"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const engineer= {...employee}
+    engineer[key] = value
+    return engineer
+}
+updateEmployeeWithKeyAndValue(employee,"Glenn","software Engineer")
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee= {...employee}
+    delete newEmployee[key];
+    return newEmployee
+}
+deleteFromEmployeeByKey(employee, key);
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+   delete employee[key];
+    return employee;
+}
+deleteFromEmployeeByKey(employee, key);
